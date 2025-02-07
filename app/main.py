d5dae32bc0a9f001a5e208ff5bc00205b64daf52
Buryi <ostap.burij@gmail.com>
2025-02-07 18:41:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,21 +10,22 @@ class Car:
 
 class CarWashStation:
     def __init__(self,
-                 distance_from_city_center,
-                 clean_power, average_rating,
-                 count_of_ratings):
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: int,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]):
+    def serve_cars(self, cars: list[Car]) -> int:
         income = 0
         for car in cars:
             income += self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         washing_price = (car.comfort_class
                          * (self.clean_power
                              - car.clean_mark)
@@ -29,7 +33,7 @@ class CarWashStation:
                              / self.distance_from_city_center))
         return round(washing_price, 1)
 
-    def wash_single_car(self, car: Car) -> int:
+    def wash_single_car(self, car: Car) -> float:
         if self.clean_power >= car.clean_mark:
             price = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
@@ -37,7 +41,7 @@ class CarWashStation:
         else:
             return 0
 
-    def rate_service(self, mark: int):
+    def rate_service(self, mark: int) -> None:
         future_average_rating = (((self.average_rating
                                   * self.count_of_ratings)
                                  + mark)
